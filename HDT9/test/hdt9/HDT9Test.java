c258fdf20079ac4011fb90802d91a2fa87c24ec5
erickDM <erickdemata@gmail.com>
2014-11-11 00:03:08 -0600

    JUnit

---
+++ HDT9/test/hdt9/HDT9Test.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hdt9;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author E. de Mata
+ */
+public class HDT9Test {
+    
+    public HDT9Test() {
+    }
+
+    /**
+     * Test of main method, of class HDT9.
+     */
+    @Test
+    public void testMain() {
+    }
+    
+}
